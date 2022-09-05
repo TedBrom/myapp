b1a3f4c65fc17dc318687a6152429c37a8cf9f9b
Ed <edwardbromilow@live.co.uk>
2022-09-05 15:55:55 +0100

    added api calls and hard coded functionality

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
 
+
+
+function getAllFilms() {
+
+   fetch('http://localhost:8080/Home/allFilms', { method: 'GET' })
+      .then(data => data.json()) // Parsing the data into a JavaScript object
+      .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+      
+  }
+  function getAFilm()
+  {
+    fetch('http://localhost:8080/Home/aFilm/1', { method: 'GET' })
+      .then(data => data.json()) // Parsing the data into a JavaScript object
+      .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+      
+  }
+  function getFilmTitle()
+  {
+    fetch('http://localhost:8080/Home/aFilm/1', { method: 'GET' })
+    .then(response => response.json())
+    .then(data => alert(data.title) ) // Displays the firstName from the API response
+  }
+
+
+
 function Contact() {
     return (
-        <h1>this is the contacts page</h1>
-        
+        <div>
+            <h1>this is the contacts page</h1>
+             <div className="button">
+                    <button onClick={getAllFilms}>Browse the catalogue</button>
+                    <button onClick={getAFilm}>Select a film</button>
+                    <button onClick={getFilmTitle}>Title</button>
+            </div>
+            
+        </div>  
     );
     
 }
--- src/aboutMe.jsx
+++ src/aboutMe.jsx
@@ -6,7 +6,6 @@ function AboutMe() {
         <div>
             <h1>this is the about me page</h1>
             <div>
-                <img src="Capture.jpg" alt="Me" width="500" height="600"></img>
                 <h3>I intend to have this as a portfolio project to demonstrate my capability and or advertise myself</h3>
             </div>
             <img src={Capture} alt = "me"/>
