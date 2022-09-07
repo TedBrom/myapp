a77688555c45b253fece7d1edb0c1a7331a56e33
Ed <edwardbromilow@live.co.uk>
2022-09-07 12:54:29 +0100

    code restructure

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,52 +2,54 @@ import React from 'react';
 
 
 
-function getAllFilms() {
+function Contact() 
+{
 
-   fetch('http://localhost:8080/Home/allFilms', { method: 'GET' })
-      .then(data => data.json()) // Parsing the data into a JavaScript object
-      .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
-      
-  }
-  function getAFilm()
-  {
-    fetch('http://localhost:8080/Home/aFilm/1', { method: 'GET' })
-      .then(data => data.json()) // Parsing the data into a JavaScript object
-      .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
-      
-  }
-  function getFilmTitle()
-  {
-    fetch('http://localhost:8080/Home/aFilm/1', { method: 'GET' })
-    .then(response => response.json())
-    .then(data => alert(data.title) ) // Displays the firstName from the API response
-  }
+    function getAllFilms() 
+    {
 
-
-  function showFilmsNicely()
-  {
-    let filmDiv = document.getElementById('filmTable')
-
-    fetch('http://localhost:8080/Home/allFilms', { method: 'GET' })
-    .then(res => res.json())
-    .then(films => 
-        {
-            films.forEach((film, index )=>
-                {if(index <=49 ){
-                    filmDiv.innerHTML += `
-                                            <tr>    
-                                                <td>${film.filmID}</td>
-                                                <td>${film.title}</td>
-                                                <td>${film.description}</td>
-                                            </tr>
-                                            `
-                                           
-                }
-                } ); 
-        })
-  }
-
-function Contact() {
+        fetch('http://localhost:8080/Home/allFilms', { method: 'GET' })
+           .then(data => data.json()) // Parsing the data into a JavaScript object
+           .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+           
+    }
+    function getAFilm()
+    {
+        fetch('http://localhost:8080/Home/aFilm/1', { method: 'GET' })
+        .then(data => data.json()) // Parsing the data into a JavaScript object
+        .then(json => alert(JSON.stringify(json))) // Displaying the stringified data in an alert popup
+           
+    }
+    function getFilmTitle()
+    {
+        fetch('http://localhost:8080/Home/aFilm/1', { method: 'GET' })
+        .then(response => response.json())
+        .then(data => alert(data.title) ) // Displays the firstName from the API response
+    }
+     
+     
+    function showFilmsNicely()
+    {
+         let filmDiv = document.getElementById('filmTable')
+     
+         fetch('http://localhost:8080/Home/allFilms', { method: 'GET' })
+         .then(res => res.json())
+         .then(films => 
+             {
+                 films.forEach((film, index )=>
+                     {if(index <=49 ){
+                         filmDiv.innerHTML += `
+                                                 <tr>    
+                                                     <td>${film.filmID}</td>
+                                                     <td>${film.title}</td>
+                                                     <td>${film.description}</td>
+                                                 </tr>
+                                                 `
+                                                
+                     }
+                     } ); 
+             })
+    }
     return (
         <div>
             <h1>Here's all the films at Film McFilmface</h1>
